6722cab6a2b444ff81ca06e8c8a7776ad0fc3b2f
fer0o <fer0osennin@gmail.com>
2022-10-17 04:59:43 -0500

    prueba

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import preguntas from './preguntas/preguntas'
+import { useState, useEffect } from 'react'
 function App () {
+  const [preguntaActual, setPreguntaActual] = useState(0)
+  const [puntuacion, setPuntuacion] = useState(0)
+  const [isFinished, setIsFinished] = useState(false)
+  // const answer = classNames ({
+  //   'bg-green-500 text-white':correct
+  //   'bg-red-500 text-white':incorrect
+  // })
+
+  const handleAnswerSubmit = (isCorrect, e) => {
+    //añadir puntuacion
+    if (isCorrect) setPuntuacion(puntuacion + 1)
+
+    //añadir estilos
+    // e.target.classList.add(isCorrect ? 'correct' : 'incorrect')
+    //cambiar siguiente pregunta
+  }
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <div className='grid grid-cols-2 border-2 border-black m-4'>
+        <div>
+          {/* lado izquierdo */}
+          <div>
+            <span>pregunta {preguntaActual + 1} de </span> {preguntas.length}
+          </div>
+          <div>{preguntas[preguntaActual].titulo}</div>
+        </div>
+        {/* lado derecho */}
+        <div className='border-l-2 border-black p-4'>
+          <div className=' flex flex-col gap-5 w-96 m-auto '>
+            {preguntas[preguntaActual].opciones.map((respuesta, idx) => (
+              <button
+                key={idx}
+                className=' border-2 border-black bg-blue-500 text-white p-2 rounded-md hover:bg-blue-300 hover:text-black  '
+                onClick={e => handleAnswerSubmit(e, respuesta.isCorrect)}
+              >
+                {respuesta.textoRespuesta}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
