f180f16feca3edc227ce63b08a0806232f2edf7d
fer0o <fer0osennin@gmail.com>
2022-10-17 02:25:53 -0500

    iniciando proyecto

---
+++ src/preguntas/preguntas.js
@@ -0,0 +1,98 @@
+const preguntas = [
+  {
+    titulo: '¿Cuál era la misión de Goku al ser enviado a la Tierra?',
+    opciones: [
+      { textoRespuesta: 'Destruir a la humanidad.', isCorrect: true },
+      { textoRespuesta: 'Proteger a la humanidad.', isCorrect: false },
+      { textoRespuesta: 'Acabar con Kame Sennin.', isCorrect: false },
+      { textoRespuesta: 'Buscar un planeta para los sayans', isCorrect: false }
+    ]
+  },
+  {
+    titulo:
+      '¿Quién le dice a Bulma que Freezer viene a la Tierra con 1000 soldados?',
+    opciones: [
+      { textoRespuesta: 'Bardok', isCorrect: false },
+      { textoRespuesta: 'Jaco', isCorrect: true },
+      { textoRespuesta: 'Broly', isCorrect: false },
+      { textoRespuesta: 'Frezeer', isCorrect: false }
+    ]
+  },
+  {
+    titulo:
+      '¿Quién es el primer Saiyajin en venir a la Tierra para buscar a Goku?',
+    opciones: [
+      { textoRespuesta: 'Vegeta.', isCorrect: false },
+      { textoRespuesta: 'Piccolo', isCorrect: false },
+      { textoRespuesta: 'Raditz', isCorrect: true },
+      { textoRespuesta: 'Bills', isCorrect: false }
+    ]
+  },
+  {
+    titulo: '¿En qué episodio Vegeta dijo «Nadie toca a mi Bulma»?',
+    opciones: [
+      { textoRespuesta: '7', isCorrect: true },
+      { textoRespuesta: '14', isCorrect: false },
+      { textoRespuesta: '12', isCorrect: false },
+      { isCorrect: '22', isCorrect: false }
+    ]
+  },
+  {
+    titulo:
+      '¿Cuántos días le dan a Goku para matar a 100 personas, después que Raditz secuestrara a Gohan?',
+    opciones: [
+      { textoRespuesta: '1 día', isCorrect: true },
+      { textoRespuesta: '5 días', isCorrect: false },
+      { textoRespuesta: '15 días', isCorrect: false },
+      { textoRespuesta: '2 días', isCorrect: false }
+    ]
+  },
+  {
+    titulo:
+      '¿Cuál de estos universo ya había sido eliminado por Zeno Sama durante el torneo de poder?',
+    opciones: [
+      { textoRespuesta: '6', isCorrect: false },
+      { textoRespuesta: '4', isCorrect: false },
+      { textoRespuesta: '9', isCorrect: true },
+      { textoRespuesta: '12', isCorrect: false }
+    ]
+  },
+  {
+    titulo:
+      '¿Quién espera conseguir el primer lugar en la competencia y luego derrotar a los dioses?',
+    opciones: [
+      { textoRespuesta: 'Gokú', isCorrect: false },
+      { textoRespuesta: 'Gohan', isCorrect: false },
+      { textoRespuesta: 'Frezeer', isCorrect: false },
+      { textoRespuesta: 'Vegeta', isCorrect: true }
+    ]
+  },
+  {
+    titulo: '¿Cuántos kilómetros recorrió Goku en el camino de la Serpiente?',
+    opciones: [
+      { textoRespuesta: '100', isCorrect: false },
+      { textoRespuesta: '1.000.000', isCorrect: true },
+      { textoRespuesta: '5.0000', isCorrect: false },
+      { textoRespuesta: '10.000.000', isCorrect: false }
+    ]
+  },
+  {
+    titulo: '¿Quién es hermano de Goku?',
+    opciones: [
+      { textoRespuesta: 'Nappa', isCorrect: false },
+      { textoRespuesta: 'Vegeta', isCorrect: false },
+      { textoRespuesta: 'Raditz', isCorrect: true },
+      { textoRespuesta: 'Trunks', isCorrect: false }
+    ]
+  },
+  {
+    titulo:
+      '¿Cómo se llama el planeta donde vive los Saiyajin del 7º universo?',
+    opciones: [
+      { textoRespuesta: 'Sadara', isCorrect: true },
+      { textoRespuesta: 'Sayan', isCorrect: false },
+      { textoRespuesta: 'Namek', isCorrect: false },
+      { textoRespuesta: 'Yardrat', isCorrect: false }
+    ]
+  }
+]
